db6a0303e2097413c303370264a3bc61e61f212a
Maxgamer959 <146353303+Maxgamer959@users.noreply.github.com>
2026-05-29 18:15:15 -0400

    Update game.js

--- game.js
+++ game.js
@@ -268,9 +268,9 @@ function initMap() {
             scrollWheelZoom: true
         }).setView(center, 4);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
-            subdomains: 'abcd',
+        // Usar mapa estándar de OpenStreetMap para máxima compatibilidad y visibilidad
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             maxZoom: 19
         }).addTo(map);
 
